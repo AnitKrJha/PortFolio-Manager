d09b5ef3dcc8668127852824e62a19fccadaf577
Anit Jha <anitkjha31@gmail.com>
2023-05-14 00:27:38 +0530

    changed the loading spinner

--- src/components/Loaders/FullPageLoader.tsx
+++ src/components/Loaders/FullPageLoader.tsx
@@ -6,13 +6,19 @@ type Props = {};
 const FullPageLoader = (props: Props) => {
   return (
     <ChakraProvider>
-      <Skeleton
-        className="max-w-screen rounded  h-[calc(100vh_-_70px)]  grid place-items-center "
-        startColor="orange.600"
-        endColor="blue.700"
-        fadeDuration={1}
-        position={"relative"}
-      ></Skeleton>
+      <div className="max-w-screen rounded  h-[calc(100vh_-_70px)]  grid place-items-center bg-blue-100 ">
+        <div className="w-fit text-center">
+          <Spinner
+            height={"40px"}
+            width={"40px"}
+            thickness="4px"
+            color="blue.400"
+          ></Spinner>
+          <h1 className="text-center text-md font-semibold animate-bounce">
+            Projects Loading..
+          </h1>
+        </div>
+      </div>
     </ChakraProvider>
   );
 };
